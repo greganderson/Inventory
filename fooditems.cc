fdcf49c9db9898bcff7ee640a0a3972a69db869d
zarate <chuy8jay@gmail.com>
2014-01-31 20:27:40 -0700

    Added a get function fo fooditems

--- fooditems.cc
+++ fooditems.cc
@@ -5,7 +5,7 @@
 fooditem::fooditem()
 {
   
-  map<string, *item> *fimap = new map<string, *item>;
+  map<string, item> *fimap = new map<string, item>;
 
 }
 
@@ -14,11 +14,18 @@ fooditem::~fooditem()
   delete *fimap;
 }
 
-fooditem::addItem(string upc, item itm)
+item fooditem::addItem(string upc, item itm)
 {
   
   *fimap[upc] = itm;
 
 }
 
-fooditem::
+/** 
+ * Provided a upc, the function will return the item 
+ * corresposnding to that upc.
+ */
+item fooditem::getItem(string upc)
+{
+  return (*fimap)[upc];
+}
