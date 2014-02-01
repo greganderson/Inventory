ff6a0e74bda39ce8ae6acd22e40fa62c69c9bbbf
Greg Anderson <greg@infobytes.com>
2014-02-01 16:34:23 -0700

    Fixed constructor call in warehouses to warehouse
    Also added the start date to warehouses and pass it into each warehouse.

--- warehouses.cc
+++ warehouses.cc
@@ -12,8 +12,9 @@ using namespace std;
 /*
  * Creates a list of warehouses.
  */
-warehouses::warehouses(fooditems & f) {
+warehouses::warehouses(fooditems & f, date & startDate) {
 	*items = f;
+	*start = startDate;
 	whs = new map<string, warehouse*>;
 }
 
@@ -28,7 +29,7 @@ warehouses::~warehouses() {
  * Takes a name of a warehouse and adds it to the list of warehouses.
  */
 void warehouses::addWarehouse(string name) {
-	(*whs)[name] = new warehouse(name, *items);
+	(*whs)[name] = new warehouse(name, *items, *start);
 }
 
 /*
